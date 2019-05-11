4c7f75ab63ffaf363c6a33af15a2f034830ec929
Sajesh Bahing <sajesh.bahing22@gmail.com>
2019-05-12 03:55:36 +1000

    week 8 evidence

--- week7/start.sh
+++ week7/start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 export IMAGE_TAG="1.4.0"
-export COMPOSE_PROJECT_NAME="FirstNetwork"
+export COMPOSE_PROJECT_NAME="firstnetwork"
 
 GENERATE_CONFIG=$1
 GENERATE_CONFIG_ONLY=$2
 
 if [ ! -z "$GENERATE_CONFIG" ] && [ $GENERATE_CONFIG == "gen" ]
 then
-	export CHANNEL_NAME="FirstChannel"
+	export CHANNEL_NAME="firstchannel"
 
 	cryptogen generate --config=./crypto-config.yaml
 
@@ -25,4 +25,6 @@ if [ -z "$GENERATE_CONFIG_ONLY" ]
 then
 	docker-compose -f docker-compose-cli.yaml up -d
 fi
+
+#peer channel create -o greenwood.deakin.edu.au:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/deakin.edu.au/orderers/greenwood.deakin.edu.au/msp/tlscacerts/tlsca.deakin.edu.au-cert.pem
 
--- week7/stop.sh
+++ week7/stop.sh
@@ -1,8 +1,17 @@
-docker stop orderer.example.com
-docker rm orderer.example.com
+#docker stop orderer.example.com
+#docker rm orderer.example.com
+docker stop cli
+docker rm cli
+
+docker stop greenwood.deakin.edu.au
+docker rm greenwood.deakin.edu.au
 
 docker stop peer0.peer.example.com 
 docker rm peer0.peer.example.com
 
 docker stop peer1.peer.example.com
 docker rm peer1.peer.example.com
+
+docker volume rm firstnetwork_greenwood.deakin.edu.au
+docker volume rm firstnetwork_peer0.peer.example.com
+docker volume rm firstnetwork_peer1.peer.example.com
