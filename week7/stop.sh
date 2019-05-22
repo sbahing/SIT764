e5a66c60fdf1651f0b7b719a4133a182951ea194
Sajesh Bahing <sajesh.bahing22@gmail.com>
2019-05-23 00:44:27 +1000

    week 10 evidence

--- week7/stop.sh
+++ week7/stop.sh
@@ -1,20 +1,24 @@
 #docker stop orderer.example.com
 #docker rm orderer.example.com
 docker stop cli
-docker rm cli
+
+#docker rm cli
 
 docker stop greenwood.deakin.edu.au
-docker rm greenwood.deakin.edu.au
+
+#docker rm greenwood.deakin.edu.au
 
 docker stop peer0.peer.example.com 
-docker rm peer0.peer.example.com
+
+#docker rm peer0.peer.example.com
 
 docker stop peer1.peer.example.com
-docker rm peer1.peer.example.com
 
-docker volume rm firstnetwork_greenwood.deakin.edu.au
-docker volume rm firstnetwork_peer0.peer.example.com
-docker volume rm firstnetwork_peer1.peer.example.com
+#docker rm peer1.peer.example.com
 
-rm -rf channel-artifacts
-rm -rf crypto-config
+#docker volume rm firstnetwork_greenwood.deakin.edu.au
+#docker volume rm firstnetwork_peer0.peer.example.com
+#docker volume rm firstnetwork_peer1.peer.example.com
+
+# rm -rf channel-artifacts
+# rm -rf crypto-config
